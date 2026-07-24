8e6bc198217c57b6d135a76eb0326fb72cff497b
Tahoor Khalid <tahoor.haseeb8@gmail.com>
2026-07-24 09:37:29 +0500

    Upgraded reverse complement generator using biopython

---
+++ Reverse_Complement_Generator_Biopython.py
@@ -0,0 +1,9 @@
+from Bio import SeqIO
+record = SeqIO.read("sequence.fasta", "fasta") #Read the fasta file
+sequence = record.seq  #Extract the DNA equence
+reverse = sequence.reverse_complement() #Generate the reverse complement
+
+print(f"Sequence ID: {record.id}")
+print(f"Description: {record.description}")
+print(f"Original Sequence: {sequence}")
+print(f"Reverse Complement: {reverse}")
